8567b2c0c999c7bb543d37e8d6e6ffcc1578b2c7
mageeus <mateusk747@gmail.com>
2023-09-20 18:47:50 -0300

    first commit

--- ArtLovers.Model/Interfaces/IRepositoryPessoa.cs
+++ ArtLovers.Model/Interfaces/IRepositoryPessoa.cs
@@ -8,66 +8,7 @@ using System.Threading.Tasks;
 
 namespace ArtLovers.Model.Interfaces
 {
-    public class IRepositoryPessoa : IRepositoryBase<Pessoa>
+    public interface IRepositoryPessoa : IRepositoryBase<Pessoa>
     {
-        public Pessoa Alterar(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Pessoa> AlterarAsync(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Excluir(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Excluir(params object[] variavel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ExcluirAsync(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ExcluirAsync(params object[] variavel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Pessoa Incluir(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Pessoa> IncluirAsync(Pessoa obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Pessoa SelecionaPK(params object[] variavel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Pessoa> SelecionarPKAsync(params object[] variavel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Pessoa> SelecionarTodos()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Pessoa>> SelecionarTodosAsync()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
